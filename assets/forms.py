03e1098f571e6f0e5f1aa6bbd8936c3d718c5af4
Branislav Benkov <c37none@gmail.com>
2024-10-04 22:32:36 +0300

    asset - status

--- assets/forms.py
+++ assets/forms.py
@@ -5,4 +5,4 @@ from .models import Asset
 class AssetForm(forms.ModelForm):
     class Meta:
         model = Asset
-        fields = ['name']
+        fields = ['name', 'status']
--- assets/models.py
+++ assets/models.py
@@ -4,4 +4,5 @@ from django.db import models
 # Create your models here.
 class Asset(models.Model):
     name = models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
 
