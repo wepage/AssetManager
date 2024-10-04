8ee46d9a7e69ff2c619043388c7831770550a715
Branislav Benkov <c37none@gmail.com>
2024-10-04 20:52:13 +0300

    list assets

--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def list(request):
+    # list all assets
+    return render(request, template_name='list_assets.html')
+
+
+def create(request):
+    pass
+
+
+def manage(request):
+    pass
