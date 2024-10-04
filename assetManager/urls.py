e58dede66e913ed52e45cf732d8f3e66fc4f6b3e
Branislav Benkov <c37none@gmail.com>
2024-10-04 21:01:46 +0300

    list assets page and function

--- assetManager/urls.py
+++ assetManager/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('assets/', include('assets.urls.py'))
+    path('assets/', include('assets.urls'))
 ]
